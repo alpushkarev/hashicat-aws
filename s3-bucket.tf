582288c9e61d0b39eb56ff25675e572ec14d8eea
Aleksandr Pushkarev <alpushkarev@mail.com>
2022-10-06 15:02:07 +0000

    Add  s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,4 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/AndrePimentel-test/s3-bucket/aws"
-  version = "2.8.0"
-  bucket_prefix = var.prefix
+  source  = "app.terraform.io/alpushkarev/s3-bucket/aws"
+  version = "3.4.0"
 }
